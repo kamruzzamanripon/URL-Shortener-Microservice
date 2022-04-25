8649e5b7964863f800279c54f99324abca249008
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-04-26 02:26:53 +0600

    make it better 1

--- server.js
+++ server.js
@@ -1,130 +1,124 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-
 const dns = require('dns');
+const urlparser = require('url')
+
 const app = express();
 
 
 
-// Basic Configuration 
+var router = express.Router;
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-mongoose.connect(process.env.MONGO_DB_LINK);
-// schema for url
-const shortUrlSchema = new mongoose.Schema({
-  original_url: String,
-  short_url: Number
+/** this project needs a db !! **/
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology:true });
+
+// var db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function() {
+//   console.log("we're connected!");
+// });
+
+//Schema n Model
+var schema = new mongoose.Schema({
+  url: String
 });
-// schema into model
-const ShortUrl = mongoose.model('ShortUrl', shortUrlSchema );
+var Url = mongoose.model("Url", schema);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-app.use(bodyParser.urlencoded({extended: false}));
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+//mycode
+// app.post("/api/shorturl/new", function(req, res) {
+//   let urlRegex = /https:\/\/www.|http:\/\/www./g;
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+//   dns.lookup(req.body.url.replace(urlRegex, ""), (err, address, family) => {
+//     if (err) {
+//       res.json({"error":"invalid URL"});
+//     } else {
+//       urlModel
+//         .find()
+//         .exec()
+//         .then(data => {
+//           new urlModel({
+//             id: data.length + 1,
+//             url: req.body.url
+//           })
+//             .save()
+//             .then(() => {
+//               res.json({
+//                 original_url: req.body.url,
+//                 short_url: data.length + 1
+//               });
+//             })
+//             .catch(err => {
+//               res.json(err);
+//             });
+//         });
+//     }
+//   });
+// });
 
-// find one by orginal url
-const findOneByOrginalUrl = (url, done) => {
-  ShortUrl.findOne({original_url: url}, (err, doc) =>{
-    if (err) return done(err);
-    done(null, doc);
-  });
-}
-// find one by short url
-const findOneByShortUrl = (shortUrl, done) => {
-  ShortUrl.findOne({short_url: shortUrl}, (err, doc) => {
-    if(err) return done(err);
-    done(null, doc);
-  });
-}
-// create and save url
-const createAndSaveUrl = (url, done) => {
-  ShortUrl.count((err, docsLenght) => {
-    if(err) return done(err);
-    // first entity
-    if (docsLenght == 0){
-      new ShortUrl({original_url: url, short_url: 0})
-        .save((err, doc) => {
-          if(err) return done(err);
-          done(null, {original_url: doc.original_url, short_url: doc.short_url});
-        });
-    }
-    else {
-      new ShortUrl({ original_url: url, short_url: docsLenght})
-      .save((err, doc) => {
-        if(err) return done(err);
-        done(null, {original_url: doc.original_url, short_url: doc.short_url});
-      });
-    }
-  } );
-}
-// test valid url
-const testValidUrl = (url, done) => {
-  if ( /^https?:\/\/(w{3}.)?[\w-]+.com(\/\w+)*/.test(url) ){
-    dns.lookup(url.replace(/^https?:\/\//, ''), (err, address, family) => {
-      if(err) return done(err);
-    done(null, address);
-    });
-  }
-  else
-    done(null, null);
-}
-// api new short url
-app.post('/api/shorturl/new', (req, res) => {
-  testValidUrl(req.body.url, (err, address) => {
-    if(err) return res.json(err);
-    if (address == null)
-      return res.json({error: 'invalid URL'});
-    
-    findOneByOrginalUrl(req.body.url, (err, data) => {
-      if (err) return res.json(err);
-      // if url exists alredy
-      if (data){
-        res.json({original_url: data.original_url, short_url: data.short_url});
-      }
-      else {
-        createAndSaveUrl(req.body.url, (err, doc) => {
-          if (err) return res.json(err);
-          res.json(doc);
-        });
+app.post('/api/shorturl', function(req,res){
+    console.log(req.body);
+    const bodyurl = req.body.url;
+
+    const something = dns.lookup(urlparser.parse(bodyurl).hostname, (error, address)=>{
+      if(!address){
+        res.json({error: "Invalid URL"})
+      }else{
+        const url = new Url({url:bodyurl})
+        url.save((err, data)=>{
+          res.json({
+            original_url: data.url,
+            short_url: data.id
+          })
+        })
       }
     })
-  });
-  
-  // res.json({ orginal_url: req.body.url, short_url: 1 });
+})
+
+app.get("/api/shorturl/:id", function(req, res) {
+  const id = req.params.id;
+  Url.findById(id, (err, data)=>{
+    if(!data){
+      res.json({error: "Invalid URL"})
+    }else{
+      res.redirect(data.url)
+    }
+  })
 });
-// api get short url
-app.get('/api/shorturl/:shortUrl', (req, res) => {
-  findOneByShortUrl(req.params.shortUrl, (err, doc) => {
-    if (err) return res.json(err);
-    if (doc == null)
-      res.json({error: 'invalid short URL'});
-    else
-      res.redirect(doc.original_url);
-  });
-});
-  
-//again test mongode
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+
+
+//get
+// app.get("/api/shorturl/:number", function(req, res) {
+//   urlModel
+//     .find({ id: req.params.number })
+//     .exec()
+//     .then(url => {
+//       res.redirect(url[0]["url"]);
+//     });
+// });
+
+app.listen(port, function() {
+  console.log("Node.js listening ...");
 });
